627f76b906c0543cec67d8132fec2cfc92cd5b49
varsha2399 <90821883+varsha2399@users.noreply.github.com>
2021-09-17 09:45:59 +0530

    Update NewCode.java

--- Automationqsp/src/qsp/NewCode.java
+++ Automationqsp/src/qsp/NewCode.java
@@ -4,7 +4,7 @@ public class NewCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("good morning");
+System.out.print("good night Guys");
 	}
 
 }
