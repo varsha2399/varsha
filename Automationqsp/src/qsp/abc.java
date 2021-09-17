dab81c957c1471e3a19166f16157671b7e55596d
Varsha <Varsha@LAPTOP-2VPNHRIF>
2021-09-17 13:03:29 +0530

    ooooooo

--- Automationqsp/src/qsp/abc.java
+++ Automationqsp/src/qsp/abc.java
@@ -1,7 +1,10 @@
 package qsp;
 
 public class abc {
-public static void main(String[] args) {
-	System.out.println("Hiiii");
-}
+
+	public static void main(String[] args) {
+		System.out.println("gm");
+
+	}
+
 }
